06d00876511a85d709b80067bf20d9bbd0feac90
Gavin McNaughton <51063174+Alashir@users.noreply.github.com>
2026-04-17 12:27:51 -0700

    Add files via upload

---
+++ COMP490SERVER.py
@@ -0,0 +1,232 @@
+from flask import Flask, request, jsonify, session
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = "secret_key_here"
+
+# In-memory "database for storing Username, Password, and pulic key"
+users = {}
+
+# In-memory "database for storing chats Structure idea:"
+#conversations = {
+#    "anoop": {
+#        "pranav": {
+#            "aes_key_for_anoop": "...",
+#            "aes_key_for_pranav": "...",
+ #           "messages": [
+ #                         "sender": "anoop",
+ #                         "message": "ENCRYPTED_BASE64"  
+ #                          ...
+ #                          ...
+ #                          ...
+#                        ]
+ #       }
+ #   }
+#}
+conversations = {}
+
+
+# SIGNUP
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    username = data['username']
+    password = data['password']
+    public_key = data['public_key']
+
+    if username in users:
+        return jsonify({"status": "fail", "message": "Username already taken"})
+
+    users[username] = {
+        "password": password,
+        "public_key": public_key
+    }
+
+    return jsonify({"status": "success", "message": "Account created successfully"})
+
+
+# LOGIN
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data['username']
+    password = data['password']
+
+    if username in users and users[username]["password"] == password:
+        session['username'] = username
+        return jsonify({"status": "success", "message": f"Access granted. Welcome {username}"})
+    
+    return jsonify({"status": "fail", "message": "Access denied"})
+
+
+
+#home page
+#gets the chat and sends them to the user
+@app.route('/conversations')
+def get_conversations():
+    if 'username' not in session:
+        return jsonify({"status": "fail", "message": "Not logged in"})
+
+    username = session['username']
+
+    user_convos = []
+    if username in conversations:
+        user_convos = list(conversations[username].keys())
+
+    return jsonify({
+        "status": "success",
+        "conversations": user_convos
+    })
+
+
+#home page
+#Starts a new chat between 2 users
+@app.route('/start_chat', methods=['POST'])
+def start_chat():
+    if 'username' not in session:
+        return jsonify({"status": "fail", "message": "Not logged in"})
+
+    data = request.json
+    user1 = session['username']
+    user2 = data['username']
+
+    if user2 not in users:
+        return jsonify({"status": "fail", "message": "User does not exist"})
+
+    # Initialize both sides
+    conversations.setdefault(user1, {})
+    conversations.setdefault(user2, {})
+
+    if user2 in conversations[user1]:
+        return jsonify({"status": "fail", "message": "Chat already exists"})
+
+    # Store encrypted AES keys
+    conversations[user1][user2] = {
+        "aes_key": data["aes_key_user1"],
+        "messages": []
+    }
+
+    conversations[user2][user1] = {
+        "aes_key": data["aes_key_user2"],
+        "messages": []
+    }
+
+    return jsonify({"status": "success", "message": "Chat created"})
+
+
+
+#home page
+#For creating a new chat sends the public keys of both users
+@app.route('/get_public_keys', methods=['POST'])
+def get_keys():
+    if 'username' not in session:
+        return jsonify({"status": "fail"})
+
+    data = request.json
+    other = data['username']
+    me = session['username']
+    
+
+    if other not in users:
+        return jsonify({"status": "fail", "message": "User does not exist"})
+
+    return jsonify({
+        "status": "success",
+        "my_public_key": users[me]["public_key"],
+        "their_public_key": users[other]["public_key"]
+    })
+
+
+
+
+
+
+
+
+
+
+#chat page
+#gets messages and sends them to the user (still encrypted)
+@app.route('/get_messages', methods=['POST'])
+def get_messages():
+    if 'username' not in session:
+        return jsonify({"status": "fail", "message": "Not logged in"})
+
+    data = request.json
+    user1 = session['username']
+    user2 = data['username']
+
+    if user1 not in conversations or user2 not in conversations[user1]:
+        return jsonify({"status": "fail", "message": "No conversation"})
+
+    return jsonify({
+        "status": "success",
+        "messages": conversations[user1][user2]["messages"]
+    })
+
+
+
+#chat page
+#gets the aes key that encrypted the messages and that is encrypted with that users public key
+@app.route('/get_aes_key', methods=['POST'])
+def get_aes_key():
+    if 'username' not in session:
+        return jsonify({"status": "fail"})
+
+    data = request.json
+    user1 = session['username']
+    user2 = data['username']
+
+    if user2 not in conversations[user1]:
+        return jsonify({"status": "fail"})
+
+    return jsonify({
+        "status": "success",
+        "aes_key": conversations[user1][user2]["aes_key"]
+    })
+
+#chat page
+#Puts the message receved from user into the conversations variable correctly
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    if 'username' not in session:
+        return jsonify({"status": "fail"})
+
+    data = request.json
+    sender = session['username']
+    receiver = data['username']
+    encrypted_msg = data['message']
+
+    msg_obj = {
+        "sender": sender,
+        "message": encrypted_msg
+    }
+
+    conversations[sender][receiver]["messages"].append(msg_obj)
+    conversations[receiver][sender]["messages"].append(msg_obj)
+
+    return jsonify({"status": "success"})
+
+
+
+
+
+
+
+
+
+
+
+# LOGOUT
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return jsonify({"message": "Logged out"})
+#source venv/bin/activate
+#https://corinna-hymnological-unlearnedly.ngrok-free.dev
+#ngrok http 5000
+#source venv/bin/activate
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ COMP490USER.py
@@ -0,0 +1,356 @@
+import os
+import base64
+import requests
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+SERVER_URL = "https://corinna-hymnological-unlearnedly.ngrok-free.dev"
+#source venv/bin/activate
+#https://corinna-hymnological-unlearnedly.ngrok-free.dev
+#ngrok http 5000
+#source venv/bin/activate
+# Shared session so cookies persist between requests
+session = requests.Session()
+CURRENT_USER = None
+
+#Login Page (signup)
+def signup():
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    print("Generating RSA keys...")
+    
+    
+    
+    #Generating keys and turning them into PEM becuse thats better for storing
+    #and required to send accross HTTP
+    #-----------------------------------------------------------------------------------
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+
+    public_key = private_key.public_key()
+
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    #-------------------------------------------------------------------------------------
+    
+    
+    
+
+    # Save private key locally in a .pem file (not secure yet)
+    with open(f"{username}_private.pem", "w") as f:
+        f.write(private_pem.decode())
+
+    # Send username, password, and public_key public key to server
+    response = session.post(
+        f"{SERVER_URL}/signup",
+        json={
+            "username": username,
+            "password": password,
+            "public_key": public_pem.decode()
+        }
+    )
+    print(response.json()["message"])
+
+#Login Page (login)
+def login():
+    global CURRENT_USER
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    response = session.post(
+        f"{SERVER_URL}/login",
+        json={
+            "username": username,
+            "password": password
+        }
+    )
+
+    data = response.json()
+    print(data["message"])
+
+    if data["status"] == "success":
+        CURRENT_USER = username
+        # send to home page if login successful
+        home_page()
+        
+        
+        
+    
+
+
+
+#home page
+def home_page():
+    while True:
+        response = session.get(f"{SERVER_URL}/conversations")
+        data = response.json()
+
+        if data["status"] == "fail":
+            print("Not logged in")
+            return
+
+        print("\n=== Your Conversations ===")
+
+        if not data["conversations"]:
+            print("No conversations yet.")
+        else:
+            for user in data["conversations"]:
+                print("-", user)
+
+        print("\nOptions:")
+        print("1. Open chat")
+        print("2. Start new chat")
+        print("3. Logout")
+
+        choice = input("Choose: ")
+
+        if choice == "1":
+            other = input("Enter username: ")
+            chat(other)
+
+        elif choice == "2":
+            new_chat()
+
+        elif choice == "3":
+            res = session.get(f"{SERVER_URL}/logout")
+            print(res.json()["message"])
+            return  # exit home page → back to main menu
+
+        else:
+            print("Not a valid option")
+        
+        
+        
+
+
+#home page
+#makeing an new chat
+def new_chat():
+    other = input("Enter username to start chat: ")
+
+
+
+    # Step 1: Get public keys
+    res = session.post(f"{SERVER_URL}/get_public_keys", json={"username": other})
+    data = res.json()
+    
+    # Step 2: Check user exists
+    if data["status"] == "fail":
+        print(data["message"])
+        return
+
+    my_pub = data["my_public_key"]
+    their_pub = data["their_public_key"]
+
+    # Step 3: Generate AES key
+    aes_key = os.urandom(32)
+
+
+
+
+
+    # Step 4: duplicate AES key and encrypt with both public keys here
+    encrypted_for_me = encrypt_with_rsa(my_pub, aes_key)
+    encrypted_for_them = encrypt_with_rsa(their_pub, aes_key)
+
+    # Step 5: Send to server
+    res = session.post(f"{SERVER_URL}/start_chat", json={
+        "username": other,
+        "aes_key_user1": encrypted_for_me,
+        "aes_key_user2": encrypted_for_them
+    })
+
+    print(res.json()["message"])
+    
+    
+    
+    
+    
+    
+#home page
+#for start new chat
+#encrypts aes key with public key
+def encrypt_with_rsa(public_pem, data):
+    # Load public key from PEM string
+    public_key = serialization.load_pem_public_key(
+        public_pem.encode()
+    )
+
+    # Encrypt data (AES key)
+    encrypted = public_key.encrypt(
+        data,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    # Convert to base64 so it can be sent over JSON
+    return base64.b64encode(encrypted).decode()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+def chat(other):
+
+    # Step 1: Get AES key
+    res = session.post(f"{SERVER_URL}/get_aes_key", json={"username": other})
+    data = res.json()
+
+    if data["status"] == "fail":
+        print("Error getting AES key")
+        return
+
+    encrypted_aes = data["aes_key"]
+
+    aes_key = decrypt_with_rsa(f"{CURRENT_USER}_private.pem", encrypted_aes)
+
+    while True:
+        # Step 2: Get messages
+        res = session.post(f"{SERVER_URL}/get_messages", json={"username": other})
+        messages = res.json()["messages"]
+
+        print("\n=== Chat ===")
+        for msg in messages:
+            decrypted = decrypt_message(aes_key, msg["message"])
+            print(f"{msg['sender']}: {decrypted}")
+
+        print("\n1. Send message")
+        print("2. Refresh")
+        print("3. Back")
+
+        choice = input("Choose: ")
+
+        if choice == "1": #send message
+            text = input("Enter message: ")
+
+            encrypted = encrypt_message(aes_key, text)
+
+            session.post(f"{SERVER_URL}/send_message", json={
+                "username": other,
+                "message": encrypted
+            })
+
+        elif choice == "2":
+            continue
+
+        elif choice == "3":
+            return
+
+        else:
+            print("Invalid option")
+
+
+
+
+
+
+
+
+
+    
+#chat page
+#encrypt the message with decrypted aes key
+def encrypt_message(aes_key, plaintext):
+    iv = os.urandom(16)
+
+    cipher = Cipher(algorithms.AES(aes_key), modes.CFB(iv))
+    encryptor = cipher.encryptor()
+
+    ciphertext = encryptor.update(plaintext.encode()) + encryptor.finalize()
+
+    return base64.b64encode(iv + ciphertext).decode()
+
+
+
+
+#chat page
+#decrypt the message with decrypted aes key
+def decrypt_message(aes_key, encrypted_data):
+    raw = base64.b64decode(encrypted_data)
+
+    iv = raw[:16]
+    ciphertext = raw[16:]
+
+    cipher = Cipher(algorithms.AES(aes_key), modes.CFB(iv))
+    decryptor = cipher.decryptor()
+
+    return (decryptor.update(ciphertext) + decryptor.finalize()).decode()
+
+
+
+#chat page
+#decrypt the aes key with private key
+def decrypt_with_rsa(private_key_path, encrypted_data):
+    from cryptography.hazmat.primitives.asymmetric import padding
+
+    with open(private_key_path, "rb") as f:
+        private_key = serialization.load_pem_private_key(
+            f.read(),
+            password=None
+        )
+
+    decoded = base64.b64decode(encrypted_data)
+
+    return private_key.decrypt(
+        decoded,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+
+
+
+
+
+
+def main():
+    while True:
+        print("\n1. Login")
+        print("2. Sign Up")
+        print("3. Exit")
+
+        choice = input("Choose: ")
+
+        if choice == "1":
+            login()
+        elif choice == "2":
+            signup()
+        elif choice == "3":
+            break
+        else:
+            print("Invalid option")
+
+
+if __name__ == "__main__":
+    main()
